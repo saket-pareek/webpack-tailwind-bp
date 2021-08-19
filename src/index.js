f379a3a70e470ecce9912650e694feed0355a390
Saket Pareek <saket.wdev@gmail.com>
2021-08-19 09:06:49 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+console.log('bhangi raam');
+
+console.log($('.bg-green-700'));
+
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                // Note!!! Oder is from last to first
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader', options: { importLoaders: 1 }
+                    },
+                    'postcss-loader'
+                ]
+            }
+        ]
+    },
+    
+    devServer: {
+        watchContentBase: true,
+        contentBase: path.resolve(__dirname, 'dist'),
+        open: true
+    }
+}
